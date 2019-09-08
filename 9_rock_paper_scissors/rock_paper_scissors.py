ee9ba1774499fff92748f8e88d2e9a6afd4f4426
Brad Cas <bmcasalvieri@gmail.com>
2019-09-08 00:01:10 -0400

    rock paper scissors

---
+++ 9_rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,30 @@
+from random import randint
+
+player = input("rock, paper, or scissors? ")
+player = player.lower()
+
+choices = ["rock", "paper", "scissors"]
+random_number = randint(0, 2)
+computer = choices[random_number]
+print(f"The computer played: {computer}")
+
+if player:
+  if player == computer:
+    print("Tie!")
+  elif player == "rock":
+    if computer == "scissors":
+      print("You win!")
+    else:
+      print("Computer wins!")
+  elif player == "paper":
+    if computer == "rock":
+      print("You win!")
+    else:
+      print("Computer wins!")
+  else:
+    if computer == "paper":
+      print("You win!")
+    else:
+      print("Computer wins!")
+else:
+  print("You didn't choose rock, paper, or scissors!")
