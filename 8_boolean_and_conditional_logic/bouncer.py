5895348cf30cd990228c175e2e6f21275cb0b0ca
Brad Cas <bmcasalvieri@gmail.com>
2019-09-07 23:42:41 -0400

    first activities

---
+++ 8_boolean_and_conditional_logic/bouncer.py
@@ -0,0 +1,13 @@
+# ask for age
+age = input("How old are you? ")
+
+if age:
+  age = int(age)
+  if age >= 21:
+    print("You are good to enter and drink!")
+  elif age >= 18:
+    print("You can enter, but you need a wristband!")
+  else:
+    print("You can't come in, little one! :(")
+else:
+  print("Please enter a valid age")
