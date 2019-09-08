48f92fa3eace0a089b23d43c5843cb3cc0e04ad2
Brad Cas <bmcasalvieri@gmail.com>
2019-09-08 00:13:57 -0400

    update rock paper scissors

--- 9_rock_paper_scissors/rock_paper_scissors.py
+++ 9_rock_paper_scissors/rock_paper_scissors.py
@@ -1,32 +1,26 @@
 from random import randint
 
+# collect player choice
 player = input("rock, paper, or scissors? ")
 player = player.lower()
 
+# generate computer choice from list and print choice
 choices = ["rock", "paper", "scissors"]
 random_number = randint(0, 2)
 computer = choices[random_number]
 print(f"The computer played: {computer}")
 
-if player:
+
+# if player isn't blank and correct choice made
+  # print player wins if rock/scissors, paper/rock, scissors/paper
+  # else print computer wins
+# if player is blank or incorrect choice made, print error message
+if player and (player == "rock" or player == "paper" or player == "scissors"):
   if player == computer:
     print("Tie!")
-  elif player == "rock":
-    if computer == "scissors":
+  elif (player == "rock" and computer == "scissors") or (player == "paper" and computer == "rock") or (player == "scissors" and computer == "paper"):
       print("You win!")
-    else:
-      print("Computer wins!")
-  elif player == "paper":
-    if computer == "rock":
-      print("You win!")
-    else:
-      print("Computer wins!")
-  elif player == "scissors":
-    if computer == "paper":
-      print("You win!")
-    else:
-      print("Computer wins!")
   else:
-    print("What's that? Rock, paper, or scissors only!")
+    print("Computer wins!")
 else:
   print("You didn't choose rock, paper, or scissors!")
